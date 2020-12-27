84cbcef71668668b37e3b296ca4e57121f306fbb
badishay <badishay@gmail.com>
2020-12-27 14:06:39 +0200

    c

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1 className='text-red-200'>hello from yishai b</h1>
+          <h1 className>hello from yishai b</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
