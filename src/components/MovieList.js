3ae018e591911613ca3382a2998ca33ae9cb8c8c
badishay <badishay@gmail.com>
2020-12-27 14:12:24 +0200

    10

---
+++ src/components/MovieList.js
@@ -0,0 +1,38 @@
+import React, {useState,useEffect} from 'react'
+import MovieItem from './MovieItem'
+import MyContext from './myContext'
+
+import axios from 'axios'
+// MovieList component
+// holds the base to show the popular movies 
+
+const MovieList = (props) => {
+   
+   
+    
+    
+    return (<MyContext.Consumer>
+        {
+      context=>(
+        
+            <div className='bg-galaxy min-h-screen'>
+                <div className='container flex flex-wrap justify-center min-w-full m-0  px-2 pb-10'>
+                    {context.items.map(item=><MovieItem  key={item.id} item={item}/>)
+                     }
+                </div>
+                <div className='flex justify-center'>  
+                    <input type='button' value='load more'
+                     className='cursor-pointer w-1/2 h-14 mb-10 tracking-widest rounded opacity-70 bg-gray-700 text-gray-400 mt-1 uppercase hover:bg-gray-400 hover:text-gray-700 font-bold text-2xl focus:outline-none '
+                     onClick={()=>context.morePage()}/>
+                </div>
+        </div>
+         )
+        }
+
+  </MyContext.Consumer>
+    )
+}
+
+export default MovieList
+
+
