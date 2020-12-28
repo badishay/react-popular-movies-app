4ea66028ac534c6286babd615ed0842e239e89aa
badishay <badishay@gmail.com>
2020-12-28 17:55:26 +0200

    trailer5

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -32,7 +32,7 @@ function MovieDetails() {
         console.log(trailerKey);
     }, [])
 
-    const trailer= (<iframe className=' '   src={`https://www.youtube.com/embed/${trailerKey}`} frameborder="0" width='600' height='450' allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>)
+    const trailer= (<iframe className=' rounded-3xl'   src={`https://www.youtube.com/embed/${trailerKey}`} frameborder="0" width='600' height='450' allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>)
         
 
     return (
@@ -41,13 +41,13 @@ function MovieDetails() {
   backgroundRepeat: 'no-repeat',
   
   }}>
-      <div className='bg-gray-500 bg-opacity-70'>
+      <div className='bg-gray-500 bg-opacity-70 '>
           {/* <Image src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} className=' opacity-50 p-0  '  alt='movie'> */}
               <div className=' text-gray-900 flex flex-warp '  >
                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className=' w-1/5 h-full m-10 mt-10 shadow-2xl '   alt='movie'/>
       {showTrailer? 
-                (<div className='m-10 relative'>
-                    <input type='button' className=' border-2 bg-transparent absolute  text-white text-xl rounded-full px-3 pb-1 p-1 text-center pt-0 ' value='x' onClick={()=>setShowTrailer(false)}/>
+                (<div className='m-10 relative focus:outline-none'>
+                    <input type='button' className=' border-4  bg-black bg-opacity-70 focus:outline-none absolute text-red-500 border-red-500 font-bold hover:text-red-400 hover:border-red-400  text-xl rounded-full px-3 pb-1 p-1 text-center pt-0 ' value='x' onClick={()=>setShowTrailer(false)}/>
                     {trailer}
                     </div>)
                       
@@ -57,9 +57,9 @@ function MovieDetails() {
                         {genres.map(genre=><div className=' px-3 font-bold'>{genre.name}</div>)}
                     </div>
                     {trailerKey?
-                     <input type='button' value='trailer >' onClick={()=>setShowTrailer(true)}/>
+                     <input type='button' value='trailer >' className='px-2 rounded-full font-bold text-yellow-50 bg-red-700 pb-1 cursor-pointer hover:bg-red-600 text-center' onClick={()=>setShowTrailer(true)}/>
                      :null}
-                    {movie.overview?<p className='h-auto  tracking-wide font-semibold text-sm'><header className='font-bold mb-2 text-2xl'>overview</header> {movie.overview}</p>:null}
+                    {movie.overview?<p className='h-auto tracking-wide font-semibold text-sm'><header className='font-bold mb-2 text-2xl'>overview</header> {movie.overview}</p>:null}
                 </div>}
                 
             </div >
