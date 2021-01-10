92b14081944c6e0f8d0cc016c3a26e7fdeca0dad
badishay <badishay@gmail.com>
2021-01-10 10:59:32 +0200

    search2

---
+++ src/components/SearchResults.js
@@ -0,0 +1,41 @@
+import React, {useState,useEffect} from 'react'
+import MovieItem from './MovieItem'
+import MyContext from './myContext'
+
+
+
+const SearchResults = (props) => {
+   
+  
+    
+    
+    return (<MyContext.Consumer>
+        {
+      context=>(
+        
+            <div className='bg-galaxy min-h-screen ' >
+                <h1>hello</h1>
+                
+                <div className='uppercase fixed mt-14 cursor-default z-10 w-full text-center tracking-widest bg-black bg-opacity-80 font-thin  text-xl  pb-3' >
+                    <p className='text-red-500 '>{context.header}</p>
+            </div >
+                <div className='  flex flex-wrap justify-center  pt-20 px-2' >
+                    {context.items.map(item=><MovieItem  key={item.id} item={item}/>)
+                     }
+                </div>
+                {/* <div className='flex justify-center'>  
+                    <input type='button' value='load more'
+                     className='cursor-pointer w-1/2 h-14 mb-10 tracking-widest rounded opacity-70 bg-gray-700 text-gray-400 mt-1 uppercase hover:bg-gray-400 hover:text-gray-700 font-bold text-2xl focus:outline-none '
+                     onClick={()=>context.morePage()}/>
+                </div> */}
+        </div>
+         )
+        }
+
+  </MyContext.Consumer>
+    )
+}
+
+export default SearchResults
+
+
